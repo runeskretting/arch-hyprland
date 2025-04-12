92212b51566c566e6845ed19ae4a3d71d1143574
Rune Skretting <rune@skretting.me>
2025-04-12 20:33:55 +0200

    Enhanced install-master.sh by adding GTK theme, icon, cursor, and font configurations. Updated window rules for font settings and improved shadow properties in the decoration section.

--- install-master.sh
+++ install-master.sh
@@ -157,6 +157,11 @@ exec-once = waybar & hyprpaper & swaync
 env = XCURSOR_SIZE,24
 env = HYPRCURSOR_SIZE,24
 
+# Set GTK theme, icons, cursor and fonts
+env = GTK_THEME,Adwaita:dark
+env = XCURSOR_THEME,Adwaita
+env = GTK_FONT_NAME,Inter 10
+
 #####################
 ### LOOK AND FEEL ###
 #####################
@@ -164,112 +169,58 @@ env = HYPRCURSOR_SIZE,24
 general {
     gaps_in = 5
     gaps_out = 20
-
     border_size = 2
-
     col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
     col.inactive_border = rgba(595959aa)
-
     resize_on_border = false
     allow_tearing = false
-
     layout = dwindle
 }
 
 decoration {
     rounding = 10
     rounding_power = 2
-
     active_opacity = 1.0
     inactive_opacity = 1.0
-
-    shadow {
-        enabled = true
-        range = 4
-        render_power = 3
-        color = rgba(1a1a1aee)
-    }
-
+    drop_shadow = true
+    shadow_range = 4
+    shadow_render_power = 3
+    shadow_ignore_window = true
+    col.shadow = rgba(1a1a1aee)
     blur {
         enabled = true
         size = 3
         passes = 1
+        new_optimizations = true
         vibrancy = 0.1696
     }
 }
 
-animations {
-    enabled = true
-
-    bezier = easeOutQuint,0.23,1,0.32,1
-    bezier = easeInOutCubic,0.65,0.05,0.36,1
-    bezier = linear,0,0,1,1
-    bezier = almostLinear,0.5,0.5,0.75,1.0
-    bezier = quick,0.15,0,0.1,1
-
-    animation = global, 1, 10, default
-    animation = border, 1, 5.39, easeOutQuint
-    animation = windows, 1, 4.79, easeOutQuint
-    animation = windowsIn, 1, 4.1, easeOutQuint, popin 87%
-    animation = windowsOut, 1, 1.49, linear, popin 87%
-    animation = fadeIn, 1, 1.73, almostLinear
-    animation = fadeOut, 1, 1.46, almostLinear
-    animation = fade, 1, 3.03, quick
-    animation = layers, 1, 3.81, easeOutQuint
-    animation = layersIn, 1, 4, easeOutQuint, fade
-    animation = layersOut, 1, 1.5, linear, fade
-    animation = fadeLayersIn, 1, 1.79, almostLinear
-    animation = fadeLayersOut, 1, 1.39, almostLinear
-    animation = workspaces, 1, 1.94, almostLinear, fade
-    animation = workspacesIn, 1, 1.21, almostLinear, fade
-    animation = workspacesOut, 1, 1.94, almostLinear, fade
+# Set the font for window titles
+xwayland {
+    force_zero_scaling = true
 }
 
-dwindle {
-    pseudotile = true
-    preserve_split = true
-}
-
-master {
-    new_status = master
-}
-
+# Font configuration
 misc {
     force_default_wallpaper = -1
     disable_hyprland_logo = false
+    force_hypr_chan = false
+    force_default_font = false
+    mouse_move_enables_dpms = true
+    key_press_enables_dpms = true
+    animate_manual_resizes = true
+    animate_mouse_windowdragging = true
+    enable_swallow = true
+    swallow_regex = ^(ghostty)$
 }
 
-#############
-### INPUT ###
-#############
+# Window rules for fonts
+windowrulev2 = font-family:Inter,class:.*
 
-input {
-    kb_layout = no
-    kb_variant =
-    kb_model =
-    kb_options =
-    kb_rules =
-
-    follow_mouse = 1
-    sensitivity = 0
-
-    touchpad {
-        natural_scroll = false
-    }
-}
-
-gestures {
-    workspace_swipe = false
-}
-
-device {
-    name = epic-mouse-v1
-    sensitivity = -0.5
-}
-
-###################
+#############################
 ### KEYBINDINGS ###
-###################
+#############################
 
 $mainMod = SUPER
 
